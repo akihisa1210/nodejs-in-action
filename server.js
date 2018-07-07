e25709741e52d707cad66ede25d0cd191ba49491
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2018-07-07 15:08:19 +0900

    update server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const server = http.createServer();
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-type': 'text/plain'});
+server.on('request', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000);
-console.log('server running at http://localhost:3000/');
+});
+server.listen(3000);
+console.log('Server running at http://localhost:3000');

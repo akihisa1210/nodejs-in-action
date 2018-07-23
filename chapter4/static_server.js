294c6d5a8c4ea33550b235c26d81f6690a44694e
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2018-07-23 21:24:18 +0900

    error handling

--- chapter4/static_server.js
+++ chapter4/static_server.js
@@ -8,8 +8,26 @@ const root = __dirname;
 const server = http.createServer(function(req, res) {
   const url = parse(req.url);
   const path = join(root, url.pathname);
-  const stream = fs.createReadStream(path);
-  stream.pipe(res);
+  fs.stat(path, function(err, stat) {
+    if (err) {
+      if ('ENOENT' == err.code) {
+        res.statusCode = 404;
+        res.end('Not Found');
+      } else {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      }
+    } else {
+      res.setHeader('Content-Length', stat.size);
+
+      const stream = fs.createReadStream(path);
+      stream.pipe(res);
+      stream.on('error', function(err) {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      });
+    }
+  });
 });
 
 server.listen(3000);

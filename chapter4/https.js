719a38bc804796ae2d0fa8674dd32e1aaa76d7d3
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2018-07-25 21:41:57 +0900

    add comment how to generate key

--- chapter4/https.js
+++ chapter4/https.js
@@ -2,6 +2,9 @@
  * HTTPS server
  * create a secret key (key.pem) and certificate (key-cert.pem)
  * for https connection
+ *
+ * openssl genrsa 1024 > key.pem
+ * openssl req -x509 -new -key key.pem > key-cert.pem
  */
 
 const https = require('https');

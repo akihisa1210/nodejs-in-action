ae74e427272281bf0e4fdcf2e43557133e8f7666
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2018-08-04 14:26:56 +0900

    chapter5: redis with list, set

--- chapter5/redis/redis.js
+++ chapter5/redis/redis.js
@@ -16,5 +16,44 @@ client.set('color', 'red', redis.print);
 
 client.get('color', (err, values) => {
   if (err) throw err;
-  console.log('Get: ' + values);
+  console.log('get: ');
+  console.log(values);
+});
+
+client.hmset('camping', {
+  'shelter': '2-person tent',
+  'cooking': 'campstove',
+}, redis.print);
+
+client.hget('camping', 'cooking', (err, values) => {
+  if (err) throw err;
+  console.log('hget: ');
+  console.log('Will be cooking with: ' + values);
+});
+
+client.hkeys('camping', (err, keys) => {
+  if (err) throw err;
+  console.log('hkeys:');
+  for (let key of keys) {
+    console.log(key);
+  };
+});
+
+client.lpush('tasks', 'Paint the bikeshed red.', redis.print);
+client.lpush('tasks', 'Paint the bikeshed green.', redis.print);
+client.lrange('tasks', 0, -1, (err, items) => {
+  if (err) throw err;
+  console.log('lrange:');
+  for (let item of items) {
+    console.log(item);
+  }
+});
+
+client.sadd('ip.addresses', 'address1', redis.print);
+client.sadd('ip.addresses', 'address1', redis.print);
+client.sadd('ip.addresses', 'address2', redis.print);
+client.smembers('ip.addresses', (err, members) => {
+  if (err) throw err;
+  console.log('smembers:');
+  console.log(members);
 });
